51cc50800ff77fa16f17f609fb908209b51d7bd2
HeriveltoCandeia <herivelto.candeia@yahoo.com.br>
2022-07-27 01:32:06 -0300

    Ajustes 26/07/2022

---
+++ src/main/java/br/pucminas/tcc/config/ResourceServerConfig.java
@@ -0,0 +1,26 @@
+package br.pucminas.tcc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		//permitir apenas usuários da role USER
+		//** na url significa qualquer coisa
+		http
+			.authorizeRequests()
+			.antMatchers("/clientesRest/**","medicamentosRest/**").authenticated()
+			.antMatchers("/examesRest/**").hasRole("USER")
+			.anyRequest().denyAll(); // para qualquer outra, negue todas.
+		//.antMatchers("/clientesRest/**").hasRole("USER"); apenas determinada role
+		//.antMatchers("/clientesRest/**").hasAnyRole("USER", "ADMIN"); => várias roles
+		//.antMatchers("/clientesRest/**").permitAll(); => permite qualquer acionamento
+	}
+
+}
