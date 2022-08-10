68afc71699abf50a7dbd18accfac50d2fee753d4
HeriveltoCandeia <herivelto.candeia@yahoo.com.br>
2022-08-10 19:19:32 -0300

    Ajustes 10/08/2022

--- src/main/java/br/pucminas/tcc/util/erros/resource/ResourceExceptionHandler.java
+++ src/main/java/br/pucminas/tcc/util/erros/resource/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import javax.validation.ConstraintViolationException;
 import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -34,6 +35,12 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
 	}
 
+	@ExceptionHandler(InvalidGrantException.class)
+	public ResponseEntity<StandardError> invalidGrantException(InvalidGrantException e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), System.currentTimeMillis(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+	}	
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException e, HttpServletRequest request) {
 		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), System.currentTimeMillis(), "Erro na validação dos campos.");
